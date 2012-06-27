7a0383dadfd644a1a8f25576d5fc112dbdcf073b
Simon Horman <horms@verge.net.au>
2012-06-27 10:40:17 +0900

    ovs-util: Enable decoding of Open Flow 1.1 & 1.2 Queue Stats Response
    Signed-off-by: Simon Horman <horms@verge.net.au>
    
    ---
    
    v5
    * Initial post

--- lib/ofp-util.c
+++ lib/ofp-util.c
@@ -966,6 +966,8 @@ static const struct ofputil_msg_type ofputil_msg_types[] = {
                   0, sizeof(struct ofp11_table_stats)),
     OFPST11_REPLY(OFPST_PORT, OFPST_PORT,
                   0, sizeof(struct ofp11_port_stats)),
+    OFPST11_REPLY(OFPST_QUEUE, OFPST_QUEUE,
+                  0, sizeof(struct ofp11_queue_stats)),
     OFPST11_REPLY(OFPST_PORT_DESC, OFPST_PORT_DESC,
                   0, sizeof(struct ofp11_port)),
 #undef OFPST11_REPLY
@@ -987,6 +989,8 @@ static const struct ofputil_msg_type ofputil_msg_types[] = {
                   0, sizeof(struct ofp12_table_stats)),
     OFPST12_REPLY(OFPST_PORT, OFPST_PORT,
                   0, sizeof(struct ofp11_port_stats)),
+    OFPST12_REPLY(OFPST_QUEUE, OFPST_QUEUE,
+                  0, sizeof(struct ofp11_queue_stats)),
     OFPST12_REPLY(OFPST_PORT_DESC, OFPST_PORT_DESC,
                   0, sizeof(struct ofp11_port)),
 #undef OFPST12_REPLY
