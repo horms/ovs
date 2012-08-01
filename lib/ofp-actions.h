f578f9cfd8eefa078331c237cb5c80fdb60d7be0
Isaku Yamahata <yamahata@valinux.co.jp>
2012-08-02 00:24:08 +0900

    ofp-actions: export OVSINST_OFPIT11_xxx
    They will be used by ofp-parser.c.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    (cherry picked from commit a64f0b0fcd00da464aa87a93d303f7564a29e1a1)
    
    Conflicts:
    	lib/ofp-actions.c
    
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- lib/ofp-actions.h
+++ lib/ofp-actions.h
@@ -594,4 +594,39 @@ void ofpact_update_len(struct ofpbuf *, struct ofpact *);
 void ofpact_nest(struct ofpbuf *ofpacts, const struct ofpact *ofpact);
 struct ofpact *ofpact_unnest(struct ofpbuf *ofpacts);
 
+/* OpenFlow 1.1 instructions. */
+
+#define OVS_INSTRUCTIONS                                    \
+    DEFINE_INST(OFPIT11_GOTO_TABLE,                         \
+                ofp11_instruction_goto_table,     false,    \
+                "goto_table")                               \
+                                                            \
+    DEFINE_INST(OFPIT11_WRITE_METADATA,                     \
+                ofp11_instruction_write_metadata, false,    \
+                "write_metadata")                           \
+                                                            \
+    DEFINE_INST(OFPIT11_WRITE_ACTIONS,                      \
+                ofp11_instruction_actions,        true,     \
+                "write_actions")                            \
+                                                            \
+    DEFINE_INST(OFPIT11_APPLY_ACTIONS,                      \
+                ofp11_instruction_actions,        true,     \
+                "apply_actions")                            \
+                                                            \
+    DEFINE_INST(OFPIT11_CLEAR_ACTIONS,                      \
+                ofp11_instruction,                false,    \
+                "clear_actions")
+
+enum ovs_instruction_type {
+#define DEFINE_INST(ENUM, STRUCT, EXTENSIBLE, NAME) OVSINST_##ENUM,
+    OVS_INSTRUCTIONS
+#undef DEFINE_INST
+};
+
+enum {
+#define DEFINE_INST(ENUM, STRUCT, EXTENSIBLE, NAME) + 1
+    N_OVS_INSTRUCTIONS = OVS_INSTRUCTIONS
+#undef DEFINE_INST
+};
+
 #endif /* ofp-actions.h */
