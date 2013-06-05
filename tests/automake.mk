3b587e621863ea59089b8670447794e7e6ae8610
Joe Stringer <joe@wand.net.nz>
2013-06-05 13:36:59 +0900

    Add MPLS recirculation tests
    This patch introduces a python script to generate about 1500 tests for
    permutations of mpls_push,mpls_pop,dec_mpls_ttl,dec_ttl where
    recirculation occurs up to (and including) three times.
    
    Signed-off-by: Joe Stringer <joe@wand.net.nz>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    
    ---
    
    v16
    * No change
    
    v15
    * Add additional flow removal tests to cover double,multiple recirculation
    
    v14
    * Allow testing remote datapaths
     - Expose egress bridge, datapath under test, destination mac parameters
    * Add option to remove flows after each test
    
    v13
    * Use the new force-miss-model config option to test MPLS on ofproto-dpif
      through the code paths for both handle_flow_miss_with_facet() and
      handle_flow_miss_without_facet().
    
    v12
    * First post

--- tests/automake.mk
+++ tests/automake.mk
@@ -327,6 +327,7 @@ CHECK_PYFILES = \
 	tests/test-jsonrpc.py \
 	tests/test-ovsdb.py \
 	tests/test-reconnect.py \
+	tests/test-mpls.py \
 	tests/MockXenAPI.py \
 	tests/test-unix-socket.py \
 	tests/test-unixctl.py \
