b606704a2b0f1af179c1df8b9cc237ec654699a4
Simon Horman <horms@verge.net.au>
2013-11-11 10:57:04 +0900

    ofproto: Honour Table Mod settings for table-miss handling
    This reworks lookup of rules for both table 0 and table action translation.
    The result is that Table Mod settings, which can alter the miss-behaviour
    of tables, including table 0, on a per-table basis may be honoured.
    
    Previous patches proposed by myself which build on earlier merged patches
    by Andy Zhou implement the ofproto side of Table Mod. So with this patch
    the feature should be complete.
    
    Neither this patch, nor any other patches it builds on, alter the default
    behaviour of Open vSwitch. And in particular the OpenFlow1.1 behaviour is
    the default regardless of which OpenFlow version is negotiated between the
    switch and the controller.
    
    An implementation detail, which lends itself to future work, is the
    handling of OFPTC_TABLE_MISS_CONTINUE. If a table has this behaviour set by
    Table Mod and a miss occurs then a loop is created, skipping to the next
    table. It is quite easy to create a situation where this loop covers ~255
    tables which is very expensive as the lookup for each table involves taking
    locks, amongst other things.
    
    Cc: Andy Zhou <azhou@nicira.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    
    ---
    v2
    * Use ofproto->up.n_tables instead of N_TABLES as the number
      of tables present.
    * Do not use the presence of rules in a table to indicate that it is
      present. Instead treat all tables, other than TBL_INTERNAL, that
      exist in ofproto.up as present. This simplifies things slightly
      and seems to be a more logical approach.
    * As pointed out by Yamamoto-san
      - Read config for each table that is missed rather
        than just the first one. A logic bug that somehow I was blind to.

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -1847,6 +1847,7 @@ xlate_table_action(struct xlate_ctx *ctx,
         struct rule_dpif *rule;
         ofp_port_t old_in_port = ctx->xin->flow.in_port.ofp_port;
         uint8_t old_table_id = ctx->table_id;
+        enum rule_dpif_lookup_verdict verdict;
 
         ctx->table_id = table_id;
 
@@ -1854,29 +1855,37 @@ xlate_table_action(struct xlate_ctx *ctx,
          * original input port (otherwise OFPP_NORMAL and OFPP_IN_PORT will
          * have surprising behavior). */
         ctx->xin->flow.in_port.ofp_port = in_port;
-        rule_dpif_lookup_in_table(ctx->xbridge->ofproto,
-                                  &ctx->xin->flow, &ctx->xout->wc,
-                                  table_id, &rule);
+        verdict = rule_dpif_lookup_from_table(ctx->xbridge->ofproto,
+                                              &ctx->xin->flow, &ctx->xout->wc,
+                                              &ctx->table_id, &rule);
         ctx->xin->flow.in_port.ofp_port = old_in_port;
 
         if (ctx->xin->resubmit_hook) {
             ctx->xin->resubmit_hook(ctx->xin, rule, ctx->recurse);
         }
 
-        if (!rule && may_packet_in) {
-            struct xport *xport;
+        switch (verdict) {
+        case RULE_DPIF_LOOKUP_VERDICT_MATCH:
+            break;
+        case RULE_DPIF_LOOKUP_VERDICT_CONTROLLER:
+            if (may_packet_in) {
+                struct xport *xport;
 
-            /* XXX
-             * check if table configuration flags
-             * OFPTC_TABLE_MISS_CONTROLLER, default.
-             * OFPTC_TABLE_MISS_CONTINUE,
-             * OFPTC_TABLE_MISS_DROP
-             * When OF1.0, OFPTC_TABLE_MISS_CONTINUE is used. What to do? */
-            xport = get_ofp_port(ctx->xbridge, ctx->xin->flow.in_port.ofp_port);
-            choose_miss_rule(xport ? xport->config : 0,
-                             ctx->xbridge->miss_rule,
+                xport = get_ofp_port(ctx->xbridge,
+                                     ctx->xin->flow.in_port.ofp_port);
+                choose_miss_rule(xport ? xport->config : 0,
+                                 ctx->xbridge->miss_rule,
+                                 ctx->xbridge->no_packet_in_rule, &rule);
+            }
+            break;
+        case RULE_DPIF_LOOKUP_VERDICT_DROP:
+            choose_miss_rule(OFPUTIL_PC_NO_PACKET_IN, NULL,
                              ctx->xbridge->no_packet_in_rule, &rule);
+            break;
+        default:
+            NOT_REACHED();
         }
+
         if (rule) {
             xlate_recursively(ctx, rule);
             rule_dpif_unref(rule);
@@ -3089,7 +3098,7 @@ xlate_actions__(struct xlate_in *xin, struct xlate_out *xout)
     ctx.mpls_depth_delta = 0;
 
     if (!xin->ofpacts && !ctx.rule) {
-        rule_dpif_lookup(ctx.xbridge->ofproto, flow, wc, &rule);
+        ctx.table_id = rule_dpif_lookup(ctx.xbridge->ofproto, flow, wc, &rule);
         if (ctx.xin->resubmit_stats) {
             rule_dpif_credit_stats(rule, ctx.xin->resubmit_stats);
         }
