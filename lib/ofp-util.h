86f9f3bfb8379668d029a5da1b233963e3c276ad
Simon Horman <horms@verge.net.au>
2014-06-04 11:18:41 +0900

    ofputil: Support encoding OF1.4 flow monitor requests
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- lib/ofp-util.h
+++ lib/ofp-util.h
@@ -847,7 +847,8 @@ struct ofputil_flow_monitor_request {
 int ofputil_decode_flow_monitor_request(struct ofputil_flow_monitor_request *,
                                         struct ofpbuf *msg);
 void ofputil_append_flow_monitor_request(
-    const struct ofputil_flow_monitor_request *, struct ofpbuf *msg);
+    const struct ofputil_flow_monitor_request *, enum ofp_version version,
+    struct ofpbuf *msg);
 
 /* Abstract nx_flow_update. */
 struct ofputil_flow_update {
