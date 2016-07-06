horms/ovs

commit b41e3e99d462f7df05d60b9e24355eed9e13ab31

datapath: Define nf_connlabels_{put,get}.
Previously this was only done when connlabels were enabled in the kernel
config, even if the functions didn't exist. Fix the compile error.

Fixes: d70a6ff5d40d ("datapath: Define nf_connlabels_{put,get}.")
Reported-by: Simon Horman <simon.horman@netronome.com>
Signed-off-by: Joe Stringer <joe@ovn.org>
Acked-by: Pravin B Shelar <pshelar@nicira.com>

datapath/linux/compat/include/net/netfilter/nf_conntrack_labels.h
@@ -5,8 +5,8 @@
 #include <linux/version.h>
 #include_next <net/netfilter/nf_conntrack_labels.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0) && \
-    IS_ENABLED(CONFIG_NF_CONNTRACK_LABELS)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+#if IS_ENABLED(CONFIG_NF_CONNTRACK_LABELS)
 
 #ifndef NF_CT_LABELS_MAX_SIZE
 #define NF_CT_LABELS_MAX_SIZE ((XT_CONNLABEL_MAXBIT + 1) / BITS_PER_BYTE)
@@ -37,5 +37,13 @@ static inline void nf_connlabels_put(struct net *net)
 		net->ct.label_words = 0;
 }
 
-#endif
+#else /* CONFIG_NF_CONNTRACK_LABELS */
+static inline int nf_connlabels_get(struct net *net, unsigned int n_bits)
+{
+	return -ERANGE;
+}
+
+static inline void nf_connlabels_put(struct net *net) { }
+#endif /* CONFIG_NF_CONNTRACK_LABELS */
+#endif /* 4.3 */
 #endif /* _NF_CONNTRACK_LABELS_WRAPPER_H */

commit 6fa2372860e9c4bb03247fb8826ae92233b25242

datapath: Always override nf_connlabels_{put,get}
It may be the case that nf_connlabels_{put,get} are supplied
by the kernel even if NF_CONNTRACK_LABELS is defined. In order
to avoid compilation failure in such cases override these functions.

Fixes: b41e3e99d462 ("datapath: Define nf_connlabels_{put,get}.")
Cc: Joe Stringer <joe@ovn.org>
Signed-off-by: Simon Horman <simon.horman@netronome.com>
---
* I'm unsure if it might be better to use the supplied functions
* Compile tested only

datapath/linux/compat/include/net/netfilter/nf_conntrack_labels.h
@@ -47,12 +47,14 @@ static inline void rpl_nf_connlabels_put(struct net *net)
 #define nf_connlabels_put rpl_nf_connlabels_put
 
 #else /* CONFIG_NF_CONNTRACK_LABELS */
-static inline int nf_connlabels_get(struct net *net, unsigned int bits)
+static inline int rpl_nf_connlabels_get(struct net *net, unsigned int bits)
 {
 	return -ERANGE;
 }
+#define nf_connlabels_get rpl_nf_connlabels_get
 
-static inline void nf_connlabels_put(struct net *net) { }
+static inline void rpl_nf_connlabels_put(struct net *net) { }
+#define nf_connlabels_put rpl_nf_connlabels_put
 #endif /* CONFIG_NF_CONNTRACK_LABELS */
 #endif /* HAVE_NF_CONNLABELS_GET_TAKES_BIT */
 #endif /* _NF_CONNTRACK_LABELS_WRAPPER_H */