062b18d41713c461f9099c5c2d8639fe60a31125
Chris Mi via dev <ovs-dev@openvswitch.org>
2023-03-29 14:42:52 +0300

    compat: Add psample and tc sample action defines for older kernels
    Update kernel UAPI to support psample and the tc sample action.
    
    Signed-off-by: Chris Mi <cmi@nvidia.com>
    Reviewed-by: Roi Dayan <roid@nvidia.com>
    Acked-by: Eelco Chaudron <echaudro@redhat.com>

--- include/linux/automake.mk
+++ include/linux/automake.mk
@@ -1,12 +1,14 @@
 noinst_HEADERS += \
-	include/linux/netlink.h \
+	include/linux/gen_stats.h \
 	include/linux/netfilter/nf_conntrack_sctp.h \
+	include/linux/netlink.h \
 	include/linux/openvswitch.h \
 	include/linux/pkt_cls.h \
-	include/linux/gen_stats.h \
+	include/linux/psample.h \
+	include/linux/tc_act/tc_ct.h \
 	include/linux/tc_act/tc_mpls.h \
 	include/linux/tc_act/tc_pedit.h \
+	include/linux/tc_act/tc_sample.h \
 	include/linux/tc_act/tc_skbedit.h \
 	include/linux/tc_act/tc_tunnel_key.h \
-	include/linux/tc_act/tc_vlan.h \
-	include/linux/tc_act/tc_ct.h
+	include/linux/tc_act/tc_vlan.h
