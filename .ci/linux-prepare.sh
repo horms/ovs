59d8009d214936dc5684f3850d3c6d80daecfb4b
Simon Horman <horms@ovn.org>
2024-02-21 10:49:46 +0000

    wip: ci: Use the term hard stop in comments
    Recently OVS adopted a policy of using the inclusive naming word list v1
    [1, 2].
    
    This patch addresses the use of the term abort within the scope of
    comments in CI scripts.  It does so by using the term hard stop instead.
    
    [1] df5e5cf ("Documentation: Add section on inclusive language.")
    [2] https://inclusivenaming.org/word-lists/
    
    Signed-off-by: Simon Horman <horms@ovn.org>

--- .ci/linux-prepare.sh
+++ .ci/linux-prepare.sh
@@ -19,7 +19,7 @@ cd ..
 
 # Installing wheel separately because it may be needed to build some
 # of the packages during dependency backtracking and pip >= 22.0 will
-# abort backtracking on build failures:
+# perform a hard stop rather than backtracking on build failures:
 #     https://github.com/pypa/pip/issues/10655
 pip3 install --disable-pip-version-check --user wheel
 pip3 install --disable-pip-version-check --user \
